c5dfc57e03035acd42ba6ea537d8121b1c87a938
valentino <vaprea@alumno.huergo.edu.ar>
2021-04-28 00:57:29 -0300

    agrego el ejercicio7 sin el 7.7

---
+++ ejercicio7.py
@@ -0,0 +1,67 @@
+def año_bisiesto_si_o_no(año):
+    """
+        Verifica si el año ingresado es bisiesto
+    """
+    if ((año%4 == 0 and año % 100 != 0) or año%400 == 0):
+        return True
+    else:
+        return False
+            
+def mes_a_dias(mes):
+    if mes in [4, 6, 9, 11]:
+        return 30
+    elif mes == 2:
+        return 28
+    else:
+        return 31
+
+def validar_fecha(año,mes,dia):
+    validar = True
+    if (0 > mes or mes > 12):
+        validar = False
+    if dia > 30 and mes in [4,6,9,11]:
+        validar = False
+    elif dia > 28 and mes == 2:
+        validar = False     
+    elif dia > 31:
+        validar = False
+    if validar == True:
+        print ("fecha valida")
+    else:
+        print("fecha no valida")
+        
+def dias_faltantes(año,mes,dia):
+    """
+        Esta funcion calcula la cantidad de dias
+        para completar el mes
+    """
+    if mes in [4, 6, 9, 11]:
+        diasfaltantes = 30 - dia
+    elif mes == 2:
+        diasfaltantes = 28 - dia
+    else:
+        diasfaltantes = 31 - dia
+    return diasfaltantes
+
+
+def dias_faltantes_años(año,mes,dia):
+    """
+        Esta funcion calcula la cantidad de dias
+        que faltan para completar el año
+    """
+    mes = int(mes)
+    total = 0
+    for mes in range (1, mes):
+        total += mes_a_dias(mes)
+    if año_bisiesto_si_o_no(año) == True:
+        total = (366 - dia) - total
+    else:
+        total = (365 - dia) - total
+    return total
+
+
+
+    
+        
+    
+   
