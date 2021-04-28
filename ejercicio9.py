e7d48180e0dee97af46ced1c1423a0a140e08267
valentino <vaprea@alumno.huergo.edu.ar>
2021-04-28 01:03:29 -0300

    agrego el ejercicio9

---
+++ ejercicio9.py
@@ -0,0 +1,8 @@
+therealpassword="islaballena"
+contra= input("ingrese la contra: ")
+if therealpassword == contra :
+    print("Bom dia usuario")
+else:
+    while contra != "aprea":
+        contra=input("Ingrese de vuelta la contraseña:  ")
+    print ("Bom dia usuario tanto tiempo")
