8b5564b187fb5e40482f4a83e29dfed5ce2e220b
valentino <vaprea@alumno.huergo.edu.ar>
2021-04-28 00:13:40 -0300

    agrego el ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+"""
+    Se ingresan 2 numero y te devuelve el producto
+"""
+name = input("ingrese nombre:")
+print("buen dia " + name)
+num1 = int(input("ingrese un numero "))
+num2 = int(input("ingrese otro numero "))
+print (num1 * num2)
