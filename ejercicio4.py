c03bcabba7f6ee8e9dc0ae085163409e912cd35a
valentino <vaprea@alumno.huergo.edu.ar>
2021-04-28 00:23:24 -0300

    agrego el ejercicio4

---
+++ ejercicio4.py
@@ -0,0 +1,7 @@
+def fahrenheit_a_celsius(fahrenheit):
+    """
+        Se ingresa un valor en fahrenheit y te lo devuelve en celsius
+    """
+    celsius = (fahrenheit -32) * 5/9
+    return celsius
+
