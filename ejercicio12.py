5d18b965d20958da1bdfcd9d1a9858edbba99f77
valentino <vaprea@alumno.huergo.edu.ar>
2021-04-28 22:17:34 -0300

    cambio totalmente el ejercicio12

--- ejercicio12.py
+++ ejercicio12.py
@@ -3,8 +3,25 @@ def contar_vocales(cadena):
         Se ingresa una cadena y la funcion devuelve
         la cantidad de vocales que hay.
     """
-    vocales = 0
-    for c in cadena:
-        if c in "aeiouAEIOU":
-            vocales = vocales + 1
-    return vocales
+    cadena.lower()
+    a = 0
+    e = 0
+    i = 0
+    o = 0
+    u = 0
+    for letra in cadena:
+        if letra == "a":
+            a += 1
+        elif letra == "e":
+            e += 1
+        elif letra == "i":
+            i += 1
+        elif letra == "o":
+            i += 1
+        elif letra == "u":
+            u += 1
+    print (f"""Cantidad de a: , {a}
+Cantidad de e: , {e}
+Cantidad de i: , {i}
+Cantidad de o: , {o}
+Cantidad de u: , {u}""")
